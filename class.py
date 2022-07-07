3ad8858699c9fe4aec6b6960beca6f55dd32f688
sree-latha123 <106506317+sree-latha123@users.noreply.github.com>
2022-07-07 12:05:07 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,19 @@
+class myprg:
+      a=20
+      b=40
+      def display(self):
+          print(self.a, self.b)
+x=myprg()
+x.display()
+
+class myprg:
+    def display(a,b):
+        i=a
+        j=b
+        print(i,j)
+x=myprg()
+myprg.display(a=23,b=45)
+
+
+
+
---
+++ example1.py
@@ -0,0 +1,20 @@
+class sample:
+ def __init__(self,name,age):
+      self.name=name
+      self.age=age
+ def printname(self):
+      print(self.name, self.age)
+class child1(sample):
+  def __init__(self, name, age):
+         self.name = name
+         self.age = age
+def printname1(self):
+      print(self.name,self.age)
+obj1=child1("sree",14)
+obj1.printname()
+obj1.printname()
+
+
+
+
+
